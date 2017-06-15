0922a5fc24d90c7037f3bbdab8a84025acf541f4
RussellStauffer <tspendragn@yahoo.com>
2017-06-15 16:39:41 -0700

    Epic

---
+++ EpicSpiesAssetTracker/EpicSpiesAssetTracker/EpicSpiesAssets.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EpicSpiesAssets.aspx.cs" Inherits="EpicSpiesAssetTracker.EpicSpiesAssets" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            font-size: medium;
+        }
+        .auto-style3 {
+            font-size: medium;
+            font-weight: normal;
+        }
+        .auto-style4 {
+            font-weight: normal;
+        }
+        .newStyle1 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: x-large;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size: medium; font-weight: normal">
+    
+        <h3>
+            <asp:Image ID="Image1" runat="server" Height="150px" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+            <br />
+            <span class="newStyle1"><strong>Asset Performance Tracker</strong></span></h3>
+        <h3><span class="auto-style3">Asset Name: </span>
+            <asp:TextBox ID="assetNameText" runat="server"></asp:TextBox>
+            <span class="auto-style4">
+            <br class="auto-style2" />
+            <br class="auto-style2" />
+            </span><span class="auto-style3">Elections Rigged: </span>
+            <asp:TextBox ID="electionsCountText" runat="server"></asp:TextBox>
+            <span class="auto-style4">
+            <br class="auto-style2" />
+            <br class="auto-style2" />
+            </span><span class="auto-style3">Acts of Subterfuge Performed: </span>
+            <asp:TextBox ID="subterfugeCountText" runat="server"></asp:TextBox>
+        </h3>
+        <p>
+            <asp:Button ID="assetButton" runat="server" OnClick="assetButton_Click" Text="Add Asset" />
+        </p>
+    
+    </div>
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ EpicSpiesAssetTracker/EpicSpiesAssetTracker/EpicSpiesAssets.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicSpiesAssetTracker
+{
+    public partial class EpicSpiesAssets : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // Initialize arrays for spy, elections and subterfuge
+                // remember to begin at zero.
+
+                string[] spyArray = new string[0];
+                int[] electionArray = new int[0];
+                int[] subterfugeArray = new int[0];
+
+                ViewState.Add("Assets", spyArray);
+                ViewState.Add("Elections", electionArray);
+                ViewState.Add("Acts", subterfugeArray);
+            }
+        }
+
+        protected void assetButton_Click(object sender, EventArgs e)
+        {
+            // Get data from ViewState
+
+            string [] spyArray = (string[]) ViewState["Assets"];
+            int [] electionArray = (int[]) ViewState["Elections"];
+            int[] subterfugeArray = (int[]) ViewState["Acts"];
+
+            // Get the new length for the expanding strings
+
+            int newLength = spyArray.Length + 1;
+
+            // resize the arrays
+
+            Array.Resize(ref spyArray, newLength);
+            Array.Resize(ref electionArray, newLength);
+            Array.Resize(ref subterfugeArray, newLength);
+
+            // Bring the data from the text fields
+
+            string assetName = assetNameText.Text;
+            int electionsRigged = int.Parse(electionsCountText.Text);
+            int actsSubterfuge = int.Parse(subterfugeCountText.Text);
+
+            int hiIndex = spyArray.GetUpperBound(0);
+
+            spyArray[hiIndex] = assetName;
+            electionArray[hiIndex] = electionsRigged;
+            subterfugeArray[hiIndex] = actsSubterfuge;
+
+            ViewState["Assets"] = spyArray;
+            ViewState["Elections"] = electionArray;
+            ViewState["Acts"] = subterfugeArray;
+
+            // Print Statistics (Total Elections Rigged, Average acts 
+            // of subterfuge, and last asset you added.
+
+            // --- calculate total elections rigged ---
+
+            int totElections = electionArray.Sum();
+
+            // --- get the average acts of subterfuge
+
+            double avgSubterfuge = subterfugeArray.Average();
+
+            //resultLabel.Text = "<br /> Average acts of subterfuge: {1}" + avgSubterfuge.ToString();
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0} <br /> Average acts of subterfuge: {1} <br /> Last Asset Added: {2}",
+                totElections.ToString(),
+                avgSubterfuge.ToString(),
+                spyArray[hiIndex]);
+
+            // clear text fields after output
+
+            assetNameText.Text = "";
+            electionsCountText.Text = "";
+            subterfugeCountText.Text = "";
+
+        }
+    }
+}
